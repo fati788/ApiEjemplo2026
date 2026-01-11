cef58218b90dd8c135707b46ccaa7dcdb196a741
fati788 <fela102@g.educaand.es>
2026-01-11 14:33:09 +0100

    primer commit

---
+++ src/main/java/com/jaroso/apiejemplo2026/entities/Task.java
@@ -0,0 +1,28 @@
+package com.jaroso.apiejemplo2026.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "tasks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrement
+    private Long id;
+
+    @Column(nullable = false , unique = true)
+    private String title;
+
+    private  String description;
+
+    @Column(nullable = false , name = "finish_date")
+    private LocalDate finishDate;
+
+
+}
